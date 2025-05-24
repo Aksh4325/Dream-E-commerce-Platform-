8183545e09f9759f00fa018177af1733f2250c16
Aksh4325 <tiwariaksh25@gmail.com>
2025-05-25 02:09:42 +0530

    app.js

---
+++ Frontend/src/app.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/product/:id" element={<ProductScreen />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+}
+
+export default App;
