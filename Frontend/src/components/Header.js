afb4cf6b0aecf187d0af0d636f7244512921595e
Aksh4325 <tiwariaksh25@gmail.com>
2025-05-25 01:44:05 +0530

    Header.js

---
+++ Frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+// frontend/src/components/Header.js
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h2>Dream E-commerce</h2>
+    </header>
+  );
+};
+
+export default Header;
