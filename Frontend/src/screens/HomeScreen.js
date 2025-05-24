f2eae8b90e356c5930c2f661aab463e4f846a348
Aksh4325 <tiwariaksh25@gmail.com>
2025-05-25 01:45:42 +0530

    HomeScreen.js

---
+++ Frontend/src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+// frontend/src/screens/HomeScreen.js
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <main>
+      <h2>Latest Products</h2>
+    </main>
+  );
+};
+
+export default HomeScreen;
