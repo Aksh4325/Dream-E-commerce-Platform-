ef5e3a619542633523844c0cafee6d2a5b5f12bf
Aksh4325 <tiwariaksh25@gmail.com>
2025-05-25 01:28:43 +0530

    Product.js

---
+++ Backend/models/Product.js
@@ -0,0 +1,36 @@
+// backend/models/Product.js
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
