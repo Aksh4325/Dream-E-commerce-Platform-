e4ebcd1d5c552952e36cdafcab1be97e86e1d43f
Aksh4325 <tiwariaksh25@gmail.com>
2025-05-25 01:43:10 +0530

    app.js

---
+++ Frontend/src/app.js
@@ -0,0 +1,12 @@
+// frontend/src/App.js
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Dream E-commerce</h1>
+    </div>
+  );
+};
+
+export default App;
