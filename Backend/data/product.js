bb4ab6a7258ba0c3547c7d1901f177c097443e09
Aksh4325 <tiwariaksh25@gmail.com>
2025-05-25 01:34:06 +0530

    product.js

---
+++ Backend/data/product.js
@@ -0,0 +1,26 @@
+// backend/data/products.js
+const products = [
+  {
+    name: "Smartphone XYZ",
+    price: 29999,
+    description: "A powerful smartphone with amazing features.",
+    image: "/images/phone.jpg",
+    countInStock: 10,
+  },
+  {
+    name: "Wireless Headphones",
+    price: 4999,
+    description: "Noise cancelling wireless headphones.",
+    image: "/images/headphones.jpg",
+    countInStock: 15,
+  },
+  {
+    name: "Gaming Laptop",
+    price: 89999,
+    description: "High performance laptop for gaming and work.",
+    image: "/images/laptop.jpg",
+    countInStock: 5,
+  },
+];
+
+export default products;
